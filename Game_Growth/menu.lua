68e5309599b200db6b1cbbecd041884735a61ec8
windsilver <33573367+windsilver@users.noreply.github.com>
2018-12-19 03:32:32 +0800

    更新

--- Game_Growth/menu.lua
+++ Game_Growth/menu.lua
@@ -1,36 +1,35 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 
-local centerX = display.contentCenterX --相對X
-local centerY = display.contentCenterY --相對Y
+centerX = display.contentCenterX --相對X
+centerY = display.contentCenterY --相對Y
 --------------------------------------------------------------------
-local bg1 = display.newImage("Image/Bg/1.png",centerX,centerY)--載入背景1
-bg1.width = centerX*2
-bg1.height = centerY*2
-bg1.alpha = 1;
+local bg = display.newImage("Image/Bg/1.png",centerX,centerY)--載入背景1	
+bg.width = centerX*2
+bg.height = centerY*2
+bg.alpha = 1	
 
-local bg2 = display.newImage("Image/Bg/2.png",centerX,centerY)--載入背景2
-bg2.width = centerX*2
-bg2.height = centerY*2
-bg2.alpha = 0;
 
-local bg3 = display.newImage("Image/Bg/3.png",centerX,centerY)--載入背景3
-bg3.width = centerX*2
-bg3.height = centerY*2
-bg3.alpha = 0;
 
-local bg4 = display.newImage("Image/Bg/4.png",centerX,centerY)--載入背景4
-bg4.width = centerX*2
-bg4.height = centerY*2
-bg4.alpha = 0;
 
 local chara = display.newImage("Image/Chara/3.png",centerX/1.4,centerY/1.4)--載入角色
 chara.width = chara.width*1.2
 chara.height = chara.height*1.2
 
-
 --------------------------------------------------------------------
-
+function move(event)
+	  if event.phase == "began"  then
+	  	MouseX = event.x;
+	  end
+	  if event.phase == "ended"  then
+	  	MouseX2 = event.x;
+	  		if(MouseX2>MouseX) then
+	  			composer.gotoScene("switch",{effect = "slideRight",time = 500})
+	  		elseif(MouseX2<MouseX) then
+	  			composer.gotoScene("switch",{effect = "slideleft",time = 500})
+			end
+	  end	  
+end
 
 
 
@@ -42,29 +41,27 @@ chara.height = chara.height*1.2
 --------------------------------------------------------------------
 function scene:create( event )
     local sceneGroup = self.view
-    sceneGroup:insert(bg1)--bg加入群組
-    sceneGroup:insert(bg2)
-    sceneGroup:insert(bg3)
-    sceneGroup:insert(bg4)
+    sceneGroup:insert(bg)--bg加入群組
     sceneGroup:insert(chara)--chara加入群組
+    Runtime:addEventListener( "touch", move )
+
 end
 
 function scene:show( event )
-    local sceneGroup = self.view
 end
 
 function scene:hide( event )
-    local sceneGroup = self.view
 end
 
 function scene:destroy( event )
-    local sceneGroup = self.view
-    composer.removeScene("menu")
+
+	composer.removeScene("menu")
 end
 
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
+
 
 return scene
